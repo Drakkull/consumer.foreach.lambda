522aadbc636ab234530123a385507bbb8f497024
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-05-15 16:40:30 -0300

    Now using lambda expression inside the main program

--- src/engine/program.java
+++ src/engine/program.java
@@ -7,6 +7,7 @@ import entities.UpperCaseName;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class program {
@@ -20,7 +21,9 @@ public class program {
             list.add(new Product("PC", 4444.00));
             list.add(new Product("Toy", 25.00));
 
-          List<String> names= list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+            Function<Product, String> func = p -> p.getName().toUpperCase();
+
+            List<String> names= list.stream().map(func).collect(Collectors.toList());
 
           names.forEach(System.out::println);
 
