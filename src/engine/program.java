2800d5dbe6e827a9bdbc38be3dfc72a329cd4ca1
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-05-15 16:35:44 -0300

    Now I'm using map / stream to convert Strings to Uppercase

--- src/engine/program.java
+++ src/engine/program.java
@@ -2,12 +2,14 @@ package engine;
 
 import entities.PriceUpdate;
 import entities.Product;
+import entities.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
-    public class program {
+public class program {
         public static void main(String[] args) {
             Locale.setDefault(Locale.US);
 
@@ -18,10 +20,9 @@ import java.util.Locale;
             list.add(new Product("PC", 4444.00));
             list.add(new Product("Toy", 25.00));
 
-            list.forEach(new PriceUpdate());
+          List<String> names= list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 
-            list.forEach(System.out::println );
-
+          names.forEach(System.out::println);
 
 
 
